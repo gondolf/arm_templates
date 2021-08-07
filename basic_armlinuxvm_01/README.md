b57c818d8a808889c1705146910e896c774df320
Gonzalo Adolfo Escajadillo Espinoza <gonzalo.adolfo.escajadillo@kyndryl.com>
2021-08-06 19:55:06 -0500

    update azure deploy button and documentation

---
+++ basic_armlinuxvm_01/README.md
@@ -0,0 +1,12 @@
+basic_armlinuxvm
+
+# [azuredeploy] (azuredeploy.json)
+ARM Template Deployment
+
+# [azuredeploy.parameters] (azuredeploy.parameters.json)
+ARM Template parameters
+
+
+[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fgondolf%2Farm_templates%2Fmaster%2Fbasic_armlinuxvm_01%2Fazuredeploy.json%3Ftoken%3DAEUEOSB5MTGAFDMHF3ZEZSLBBXLV6)
+
+
--- basic_armlinuxvm_01/deployment.ps1
+++ basic_armlinuxvm_01/deployment.ps1
@@ -1,9 +1,13 @@
+$subscriptionid = '8bacf08c-ed66-4a7f-9c82-af1ce9a68cce'
 $projectName = "website001"
 $resourceGroupName = "rsgr${projectName}"
-$location = "eastus"
-New-AzResourceGroup -Name "$resourceGroupName" -Location "$location" 
+
+Select-AzSubscription $subscriptionid
+
+# $location = "eastus"
+# New-AzResourceGroup -Name "$resourceGroupName" -Location "$location" 
+# Remove-AzResourceGroup -Name $resourceGroupName 
 
 
 New-AzResourceGroupDeployment -ResourceGroupName "$resourceGroupName" -TemplateFile "./azuredeploy.json" -TemplateParameterFile "./azuredeploy.parameters.json" -Verbose
 
-# Remove-AzResourceGroup -Name $resourceGroupName 
