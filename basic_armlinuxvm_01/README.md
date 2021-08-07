604875a0cd6b88311df2ce440493e154bedecbd0
Gonzalo Adolfo Escajadillo Espinoza <gonzalo.adolfo.escajadillo@kyndryl.com>
2021-08-06 23:15:46 -0500

    update button for private repo

--- basic_armlinuxvm_01/README.md
+++ basic_armlinuxvm_01/README.md
@@ -7,6 +7,6 @@ ARM Template Deployment
 ARM Template parameters
 
 
-[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fgondolf%2Farm_templates%2Fmaster%2Fbasic_armlinuxvm_01%2Fazuredeploy.json%3Ftoken%3DAEUEOSB5MTGAFDMHF3ZEZSLBBXLV6)
+[![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Ffappgee01.azurewebsites.net%2Fapi%2FGitHubPrivateRepoFileFetcher%3Fgithuburi%3Dhttps%3A%2F%2Fraw.githubusercontent.com%2Fgondolf%2Farm_templates%2Fmaster%2Fbasic_armlinuxvm_01%2Fazuredeploy.json%26githubaccesstoken%3D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)
 
 
--- basic_armlinuxvm_01/deployment.ps1
+++ basic_armlinuxvm_01/deployment.ps1
@@ -11,3 +11,9 @@ Select-AzSubscription $subscriptionid
 
 New-AzResourceGroupDeployment -ResourceGroupName "$resourceGroupName" -TemplateFile "./azuredeploy.json" -TemplateParameterFile "./azuredeploy.parameters.json" -Verbose
 
+<#
+$url = "https://fappgee01.azurewebsites.net/api/GitHubPrivateRepoFileFetcher?githuburi=https://raw.githubusercontent.com/gondolf/arm_templates/master/basic_armlinuxvm_01/azuredeploy.json&githubaccesstoken=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"
+[uri]::EscapeDataString($url)
+
+https%3A%2F%2Ffappgee01.azurewebsites.net%2Fapi%2FGitHubPrivateRepoFileFetcher%3Fgithuburi%3Dhttps%3A%2F%2Fraw.githubusercontent.com%2Fgondolf%2Farm_templates%2Fmaster%2Fbasic_armlinuxvm_01%2Fazuredeploy.json%26githubaccesstoken%3D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
+#>
