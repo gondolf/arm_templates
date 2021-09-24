c303027ac8bb88cf434e94cd1c9a9d8118ff583b
Gonzalo Escajadillo <gonzalo.adolfo.escajadillo@kyndryl.com>
2021-09-23 19:39:11 -0500

    Availability Sets updates

--- basic_armwindows_01/deployment.ps1
+++ basic_armwindows_01/deployment.ps1
@@ -1,24 +1,26 @@
 param (
-    [Parameter(Mandatory = $false)] [String]  $TenantId = '00193bc8-dddd-41dd-8c93-264763bc0348',
-    [Parameter(Mandatory = $false)] [String]  $Rubscriptionid = 'd9bc6363-1579-4256-8ac0-7686697ffdb4',
-    [Parameter(Mandatory = $false)] [String]  $ResourceGroupName = 'arsgraddeu1p01',
-    [Parameter(Mandatory = $false)] [string]  $TemplateFile = "./azuredeploysinglevm.json",
+    [Parameter(Mandatory = $false)] [String]  $TenantId = "78153f1a-b836-436b-b9bb-281f4fe2d1d0",
+    [Parameter(Mandatory = $false)] [String]  $Subscriptionid = "8bacf08c-ed66-4a7f-9c82-af1ce9a68cce",
+    [Parameter(Mandatory = $false)] [String]  $ResourceGroupName = 'arsgrj6eu1p01',
+    [Parameter(Mandatory = $false)] [string]  $TemplateFile = "./azuredeployNewVM-WinAvSet.json",
     [Parameter(Mandatory = $false)] [String]  $TemplateParameterFile = "./azuredeploy.parameters.json",
     [Parameter(Mandatory = $false)] [switch]  $WhatIf 
 )
 
 $DebugPreference = 'Continue'
-function Login($SubscriptionId) {
+function Login($SubscriptionId, $TenantId) {
     $context = Get-AzContext
 
     if (!$context -or ($context.Subscription.Id -ne $SubscriptionId)) {
-        Connect-AzAccount -TenantId $context.Tenant.Id
+        Connect-AzAccount -TenantId $TenantId
+        Select-AzSubscription   -subscriptionid    $SubscriptionId
         Write-Output " Authenticated in tenant: $($context.Tenant.Id)"
     } 
     else {
         Write-Output "SubscriptionId '$SubscriptionId' already connected"
     }
 }
+
 
 if ($WhatIf.IsPresent) {
     Write-Output "Whatif $($true)"
