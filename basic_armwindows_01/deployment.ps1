c039517c260960b648a9935216c410c2051c48f6
Gonzalo Escajadillo <gonzalo.adolfo.escajadillo@kyndryl.com>
2021-09-29 13:13:36 -0500

    New repo for sql allways on

--- basic_armwindows_01/deployment.ps1
+++ basic_armwindows_01/deployment.ps1
@@ -1,8 +1,8 @@
 param (
-    [Parameter(Mandatory = $false)] [String]  $TenantId = "78153f1a-b836-436b-b9bb-281f4fe2d1d0",
-    [Parameter(Mandatory = $false)] [String]  $Subscriptionid = "8bacf08c-ed66-4a7f-9c82-af1ce9a68cce",
+    [Parameter(Mandatory = $false)] [String]  $TenantId = "00193bc8-dddd-41dd-8c93-264763bc0348",
+    [Parameter(Mandatory = $false)] [String]  $Subscriptionid = "d9bc6363-1579-4256-8ac0-7686697ffdb4",
     [Parameter(Mandatory = $false)] [String]  $ResourceGroupName = 'arsgrj6eu1p01',
-    [Parameter(Mandatory = $false)] [string]  $TemplateFile = "./azuredeployNewVM-WinAvSet.json",
+    [Parameter(Mandatory = $false)] [string]  $TemplateFile = "./azuredeployWinAvailabilitySet.json",
     [Parameter(Mandatory = $false)] [String]  $TemplateParameterFile = "./azuredeploy.parameters.json",
     [Parameter(Mandatory = $false)] [switch]  $WhatIf 
 )
@@ -17,10 +17,11 @@ function Login($SubscriptionId, $TenantId) {
         Write-Output " Authenticated in tenant: $($context.Tenant.Id)"
     } 
     else {
-        Write-Output "SubscriptionId '$SubscriptionId' already connected"
+        Write-Output "SubscriptionId $($context.Subscription.Id) already connected"
     }
 }
 
+Login -SubscriptionId $SubscriptionId -TenantId $TenantId
 
 if ($WhatIf.IsPresent) {
     Write-Output "Whatif $($true)"
